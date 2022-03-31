37ffc97172f8cb453cfa0dfa6534bfe5c4ee53d3
Fares <tryharderbruhhh@gmail.com>
2022-03-31 13:11:19 +0200

    Html update

---
+++ program.py
@@ -0,0 +1,10 @@
+import sys
+
+for i in range(1,11):
+    print i
+    if i <= 5:
+       print "AHHHHHHHH"
+    elif i >= 6 and i <= 9:
+       print "WHERE ARE ALL THE BRACKETS??"
+    else:
+       print "HOW DO YOU PEOPLE READ THIS SYNTAX EASILY"
